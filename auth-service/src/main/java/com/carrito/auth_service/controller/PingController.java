f7044f3c4621ba7e9b5f1b2521bd02dca307b19d
Julio Olmedo <turlan125@gmail.com>
2025-05-10 00:15:31 -0300

    add: health check ping pong

---
+++ auth-service/src/main/java/com/carrito/auth_service/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.carrito.auth_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+
+}
